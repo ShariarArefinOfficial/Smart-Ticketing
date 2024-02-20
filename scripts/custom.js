5e239341db752c239e90647bbb0078d25b6981d8
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-02-20 06:08:51 +0600

    js Code Updated

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,3 +1,70 @@
 console.log('connected');
 
+const button=document.querySelectorAll('button');
+console.log(button);
+let priceTotal=0;
+let count=0;
+const seatAvailable=document.getElementById('seat-decrease');
+let seatDecrease=parseInt(seatAvailable.innerText);
+
+console.log(seatDecrease);
+for(let i=0;i<button.length;i++){
+    const element=button[i];
+
+    element.addEventListener('click',function(){
+        //console.log('clicked');
+        const click =element.querySelector("span");
+        const value=click.innerText;
+        //console.log(value);
+        let priceShow=550;
+
+        //One Click After This button will be disable
+        const buttonId=click.parentNode.id;
+        
+
+        //color change
+        changeColorById(buttonId);               
+        //Seat field
+       appendElementByID('seat-num',value);
+        //Class Name
+        const className='Economoy';
+        appendElementByID('class-name',className);
+        //price field
+        appendElementByID('price',priceShow);
+
+
+
+        //total price
+        priceTotal=priceShow+priceTotal;
+        showTotalById('priceVal',priceTotal)
+
+       //seat count
+       count=count+1;
+       showTotalById('seat-count',count)
+
+       //seat availablity      
+       seatDecrease=seatDecrease-1;
+       showDecreaseById('seat-decrease',seatDecrease);
+
+       const disable=document.getElementById(buttonId);
+       disable.setAttribute('disabled',true);
+
+       if(count<=4){
+
+       }
+       else if(count>4){
+        const allDisable=document.getElementsByClassName("button");
+        //console.log(allDisable);
+        allDisable.setAttribute('disabled',true);
+        alert("No more TIcket");
+       }
+
+       
+
+
+
+
+        
+    })
+}
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,2 +1,31 @@
 console.log('connected');
+//Append Value
+function appendElementByID(ElementID,valuePress){
+    const showElementFIeld=document.getElementById(ElementID);
+    const appendName=document.createElement('p');
+    appendName.innerText=valuePress;
+        showElementFIeld.appendChild(appendName);
+}
+//Inclreament Value
+function showTotalById(ElementID,valuePress){
+    const getElement=document.getElementById(ElementID);
+    getElement.innerText=valuePress;
+}
+//decreament Value
+function showDecreaseById(ElementID,valuePress){
+    const getElement=document.getElementById(ElementID);
+    getElement.innerText=valuePress;
+}
+// function showDecreaseById(ElementID){
+//     const getElement=document.getElementById(ElementID).innerText;
+//     let getValue=parseInt(getElement);
+//     getValue=getValue-1;
+//     getElement.innerText=getValue;
+// }
+
+//change color
+function changeColorById(elementID){
+    const tergatedElement=document.getElementById(elementID);
+    tergatedElement.classList.add('seat-selection-button');
+}
 
